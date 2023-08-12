46809c008cf422ba658e5a94c68ba5ad32558eb4
maurinoferreira <maurinoferreira2011@hotmail.com>
2023-08-11 23:25:49 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// Import packages
+import express  from 'express'
+import morgan from 'morgan'
+import Getstatus from './src/Getstatus.js'
+
+// App
+const app = express()
+// Morgan
+app.use(morgan('tiny'))
+// First route
+app.get('/', async (req, res) => {
+
+        const {id} = req.query;
+
+        const result = await Getstatus({id});
+
+         res.json({result});
+})
+// Starting server
+app.listen('1337')
---
+++ src/Getstatus.js
@@ -0,0 +1,45 @@
+
+import puppeteer from "puppeteer";
+const url = 'https://www.linkcorreios.com.br/?id=';
+
+
+export default async function Getstatus({id}){
+
+
+      
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.goto(`${url}${id}`);
+            const els = await page.$$('div.singlepost');
+
+            const result = new Object();
+        
+            const arr2 = [];
+           
+            for (let i = 0; i < els.length; i++) {
+                const title = await els[i].$eval('h2', h2 => h2.textContent);
+                console.log(title);
+                result.titulo = title;
+                const text = await els[i].$eval('p', p => p.textContent);
+                result.detalhes = text;
+            }
+
+            var status = await page.$$eval('ul.linha_status > li', elements=> elements.map(item=>item.textContent));
+
+              const chunks = 3;
+             
+              while(status.length > 0)
+                {
+                     const chunk = status.splice(0, chunks);
+                     arr2.push(chunk);
+                }
+
+
+            result.historico = {...arr2};
+            await browser.close();
+
+
+            return result;
+     
+}
+
